9f049607e7efac9845f78ecc96c31e8e2563a5eb
fujiten <hikaru.fujita.0903@gmail.com>
2020-10-25 16:13:05 +0900

    Finish

--- projects/10/parser.py
+++ projects/10/parser.py
@@ -1,1 +1,10 @@
 # let's go to make Parser!
+
+class JackAnalyzer:
+  def __init__(self, name):
+    self._is_directory = False
+    self.name = name
+
+  def start_analyze(self):
+    f = open(f'{self.name}.xml', "x")
+    f.close()
